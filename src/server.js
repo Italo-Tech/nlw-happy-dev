4aa97df9e595114ed53b5ac245554eecbb444992
Italo-Tech <italosantosdick@hotmail.com>
2020-06-22 12:20:56 -0300

    Finalização

--- src/server.js
+++ src/server.js
@@ -4,8 +4,14 @@ const express = require("express")
 //Pegar o express e executar no server
 const server = express()
 
+//Pegar o banco de dads
+const db = require("./database/db")
+
 // Configurar pasta pública onde está meu css/script/assets
 server.use(express.static("public"))
+
+// Habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // Utilizando template enginer nunjucks
 const nunjucks = require("nunjucks")
@@ -25,11 +31,87 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+    // req.query: Query Strings da nossa url 
+    // console.log(req.query)
+
     return res.render("create-point.html")
 })
 
+server.get("/point-error", (req, res) => {
+
+    // req.query: Query Strings da nossa url 
+    // console.log(req.query)
+
+    return res.render("point-error.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+    // req.body: o corpo do nosso formulário
+    // console.log(req.body)
+
+    // inserir dados no banco de dados 
+    const query = `
+             INSERT INTO places (
+                 image,
+                 name,
+                 address,
+                 address2,
+                 state,
+                 city,
+                 items
+             ) VALUES (?,?,?,?,?,?,?);
+         `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.render("partials/point-error.html")
+        }
+
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+
+    // Comentar este if para aparecer todos o locais
+    if (search == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
+    //Pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        // db.all(`SELECT * FROM places`, function (err, rows) { // Para consultar todos os locais
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        // mostrar a página html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total })
+    })
 })
 
 //Server(express) ouvir a porta 3000
